2e1672061c505f1b070227353f075960432e82d0
hankd11234 <hankd11234@gmail.com>
2021-01-09 18:07:33 -0500

    Beautifying

--- curl-scripts/games/update.sh
+++ curl-scripts/games/update.sh
@@ -2,8 +2,8 @@ curl "http://localhost:4741/games/${ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
---header "Authorization: Bearer ${TOKEN}" \
---data '{
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
     "game": {
       "title": "'"${TITLE}"'",
       "releaseDate": "'"${DATE}"'",
